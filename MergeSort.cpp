183a5bf99201061fb61d473a90bb4bb2921bdc53
George Cercvadze <aeonswander@gmail.com>
2017-08-10 19:25:19 +0400

    Made code more readable
    Made code more readable in merge sort algorithm by adding some line endings.

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -9,6 +9,7 @@ void merge_arrays(vector <int>& main_array, vector <int>& left_array_to_merge, v
     int left_counter = 0;
     int right_counter = 0;
     int main_counter = 0;
+    
     //While both arrays still have elements to use take the minimum element from them and
     //push it into main array.
     while(left_counter != left_array_to_merge.size() && right_counter != right_array_to_merge.size()){
@@ -42,6 +43,7 @@ void merge_arrays(vector <int>& main_array, vector <int>& left_array_to_merge, v
 vector <int> get_subarray(vector <int>& array_to_split, int left_margin, int right_margin){
     //Create the subarray
     vector <int> subarray;
+    
     //Push elements from give arrays left margin to its right margin in newly allocated array
     for(int i = left_margin; i < right_margin; i++){
         subarray.push_back(array_to_split[i]);
@@ -58,12 +60,15 @@ void merge_sort(vector <int>& array_to_sort){
     //If size is already 1 (i.e is already sorted) return.
     if(size_of_array == 1)
         return;
+    
     //Split the array into to arrays of equal length
     vector <int> left_array = get_subarray(array_to_sort, 0, size_of_array / 2);
     vector <int> right_array = get_subarray(array_to_sort, size_of_array / 2, array_to_sort.size());
+    
     //Sort each of the given array.
     merge_sort(left_array);
     merge_sort(right_array);
+    
     //Merge the sorted arrays.
     merge_arrays(array_to_sort, left_array, right_array);
 }
